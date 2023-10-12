0ce000500a0a5fc3ff25d04468604fe577ec0576
vmarialuz <vmarialuzm@gmail.com>
2023-10-12 12:44:13 -0500

    update records

--- website/views.py
+++ website/views.py
@@ -77,6 +77,21 @@ def add_record(request):
     else:
         messages.success(request, "You must be logged in...")
         return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated!")
+            return redirect('home')
+        return render(request, 'updated_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in...")
+        return redirect('home')
+
+
 
     
 
